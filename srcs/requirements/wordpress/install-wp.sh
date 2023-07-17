6f63f6330cf7544d0dd78497244cc5e6379a54b8
fkernbac <fkernbac@debian.fkernbac.42.fr>
2023-07-17 11:41:29 -0400

    wordpress stylesheet not working

--- srcs/requirements/wordpress/install-wp.sh
+++ srcs/requirements/wordpress/install-wp.sh
@@ -14,8 +14,8 @@ echo "Installing wordpress..."
 
 wp core download --allow-root;
 wp config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --allow-root;
-wp core install --url=${DOMAIN_NAME}/wordpress --title=TEST --admin_user=fkernbac --admin_password=anuba --admin_email=fra-ker@hotmail.de --skip-email --allow-root;
-wp user create fkernbac fra-ker@hotmail.de --role=author --user_pass=anuba --allow-root;
+wp core install --url=${DOMAIN_NAME} --title=TEST --admin_user=fkernbac --admin_password=anuba --admin_email=fra-ker@hotmail.de --skip-email --allow-root;
+wp user create ${WORDPRESS_DB_USER} fra-ker@hotmail.de --role=author --user_pass=${WORDPRESS_DB_PASSWORD} --allow-root;
 
 echo "Wordpress ready for connections."
 
